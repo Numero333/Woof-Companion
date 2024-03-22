bb1fd156804bfb98d2e42a8ad3dab60ebb0d722d
xNumero3 <notnumero3@gmail.com>
2024-03-22 14:41:56 +0100

    init

---
+++ Woof-Companion-MVP/Woof_Companion_MVPApp.swift
@@ -0,0 +1,17 @@
+//
+//  Woof_Companion_MVPApp.swift
+//  Woof-Companion-MVP
+//
+//  Created by François-Xavier on 22/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct Woof_Companion_MVPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
